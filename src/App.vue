dfd52e37837f84427af22784c21ebcf42aaa2c9f
Jaunty_Liu <15613469426@163.com>
2022-02-08 13:39:14 +0800

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>一个人的信息</h1>
+	<h2>姓名：{{name}}</h2>
+	<h2>年龄：{{age}}</h2>
+	<h3>工作种类：{{job.type}}</h3>
+	<h3>工作薪水：{{job.salary}}</h3>
+	<button @click="changeInfo">修改人的信息</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import {ref} from 'vue'
+	export default {
+		name: 'App',
+		setup(){
+			//数据
+			let name = ref('张三')
+			let age = ref(18)
+			let job = ref({
+				type:'前端工程师',
+				salary:'30K'
+			})
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+			//方法
+			function changeInfo(){
+				// name.value = '李四'
+				// age.value = 48
+				console.log(job.value)
+				// job.value.type = 'UI设计师'
+				// job.value.salary = '60K'
+				// console.log(name,age)
+			}
+
+			//返回一个对象（常用）
+			return {
+				name,
+				age,
+				job,
+				changeInfo
+			}
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
