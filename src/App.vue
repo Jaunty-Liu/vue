ce0ae4eac28fe8bfe6c9c35e7baefe07415484aa
Jaunty-Liu <15613469426@163.com>
2022-02-09 23:58:57 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-show="person.name">姓名：{{person.name}}</div>
+    <div>年龄：{{person.age}}</div>
+    <div v-show="person.sex">性别：{{person.sex}}</div>
+    <button @click="addSex">添加一个sex属性</button>
+    <button @click="deleteName">删除name属性</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Vue from 'vue'
+  export default{
+    name: 'App',
+    data(){
+      return{
+        person:{
+          name: '张三',
+          age: 18
+        }
+      }
+    },
+    methods: {
+      addSex(){
+        console.log(this.person.sex)
+        this.person.sex = '女'
+        console.log(this.person.sex)
+        // this.$set(this.person, 'sex', '女')
+        Vue.set(this.person, 'sex', '女')
+      },
+      deleteName(){
+        // console.log(this.person.name)
+        // delete this.person.name
+        // console.log(this.person.name)
+        // this.$delete(this.person, 'name')
+        Vue.delete(this.person, 'name')
+      }
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
