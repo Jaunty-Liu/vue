3712ab01b4dd9ed861fcdd38154dc246ef018202
Jaunty_Liu <15613469426@163.com>
2022-02-09 16:57:18 +0800

    reactive

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,60 @@
 <template>
 	<h1>一个人的信息</h1>
-	<h2>姓名：{{name}}</h2>
-	<h2>年龄：{{age}}</h2>
-	<h3>工作种类：{{job.type}}</h3>
-	<h3>工作薪水：{{job.salary}}</h3>
+	<h2>姓名：{{person.name}}</h2>
+	<h2>年龄：{{person.age}}</h2>
+	<h3>工作种类：{{person.job.type}}</h3>
+	<h3>工作薪水：{{person.job.salary}}</h3>
+	<h3>爱好：{{person.hobby}}</h3>
+	<h3>测试的数据c：{{person.job.a.b.c}}</h3>
+	<!-- reactive能监控深层数据 -->
 	<button @click="changeInfo">修改人的信息</button>
 </template>
 
 <script>
-	import {ref} from 'vue'
+	import {reactive} from 'vue'
 	export default {
 		name: 'App',
 		setup(){
 			//数据
-			let name = ref('张三')
-			let age = ref(18)
-			let job = ref({
-				type:'前端工程师',
-				salary:'30K'
+			let person = reactive({
+				name:'张三',
+				age:18,
+				job:{
+					type:'前端工程师',
+					salary:'30K',
+					a:{
+						b:{
+							c:666
+						}
+					}
+				},
+				hobby:['抽烟','喝酒','烫头']
 			})
+
+			// 另一种写法：
+			/*
+			let person = {
+				……
+			}
+			let p = reactive(person)
+			*/
 
 			//方法
 			function changeInfo(){
-				// name.value = '李四'
-				// age.value = 48
-				console.log(job.value)
-				// job.value.type = 'UI设计师'
-				// job.value.salary = '60K'
-				// console.log(name,age)
+				// reactive就不需要写value了
+				person.name = '李四'
+				person.age = 48
+				person.job.type = 'UI设计师'
+				person.job.salary = '60K'
+				person.job.a.b.c = 999
+				person.hobby[0] = '学习'
 			}
 
 			//返回一个对象（常用）
 			return {
-				name,
-				age,
-				job,
+				person,
 				changeInfo
 			}
 		}
 	}
 </script>
-
