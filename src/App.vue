48ecf00b7514d69960aa90ac62be0279212ffc94
Jaunty-Liu <15613469426@163.com>
2022-02-10 11:39:56 +0800

    vue2响应式

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,30 @@
 <template>
   <div>
-    <div v-show="person.name">姓名：{{person.name}}</div>
-    <div>年龄：{{person.age}}</div>
-    <div v-show="person.sex">性别：{{person.sex}}</div>
-    <button @click="addSex">添加一个sex属性</button>
-    <button @click="deleteName">删除name属性</button>
+    <div>爱好：{{person.hobby}}</div>
+    <button @click="updateHobby">改变第一个hobby值</button>
   </div>
 </template>
 
 <script>
-  import Vue from 'vue'
+  // import Vue from 'vue'
   export default{
     name: 'App',
     data(){
       return{
         person:{
           name: '张三',
-          age: 18
+          age: 18,
+          hobby:['学习', '吃饭']
         }
       }
     },
     methods: {
-      addSex(){
-        console.log(this.person.sex)
-        this.person.sex = '女'
-        console.log(this.person.sex)
-        // this.$set(this.person, 'sex', '女')
-        Vue.set(this.person, 'sex', '女')
-      },
-      deleteName(){
-        // console.log(this.person.name)
-        // delete this.person.name
-        // console.log(this.person.name)
-        // this.$delete(this.person, 'name')
-        Vue.delete(this.person, 'name')
+      updateHobby(){
+        // this.person.hobby[0] = '逛街'// 非响应式
+        // 第一种：
+        this.$set(this.person.hobby, 0, '逛街')
+        // 第二种：
+        this.person.hobby.splice(0, 1, '逛街')
       }
     },
   }
