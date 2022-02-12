645ccdb4652f8216bb448c3b1174ad1c081e1f3e
Jaunty-Liu <15613469426@163.com>
2022-02-12 20:57:25 +0800

    Suspense

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
 	<div class="app">
 		<h3>我是App组件</h3>
-		<Child/>
+		<Suspense>
+			<template v-slot:default>
+				<!-- 这就是真正要展示的默认的组件 -->
+				<Child/>
+			</template>
+			<template v-slot:fallback>
+				<!-- 如果默认组件未加载加载这个 -->
+				<h3>稍等，加载中...</h3>
+			</template>
+		</Suspense>
 	</div>
 </template>
 
 <script>
-	import Child from './components/Child'
+	// import Child from './components/Child'//静态引入
+	// 如果静态引入的话setup就不能返回promise因为这样组件会消失
+	import {defineAsyncComponent} from 'vue' 
+	const Child = defineAsyncComponent(()=>import('./components/Child')) //异步引入
 	export default {
 		name:'App',
 		components:{Child},
@@ -16,6 +28,6 @@
 <style>
 	.app{
 		background-color: gray;
-		padding: 10px;
+		padding: .625rem;
 	}
 </style>
--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,15 +1,23 @@
 <template>
 	<div class="child">
 		<h3>我是Child组件</h3>
-		<Son/>
+		{{sum}}
 	</div>
 </template>
 
 <script>
-	import Son from './Son'
+	import {ref} from 'vue'
 	export default {
 		name:'Child',
-		components:{Son},
+		async setup(){
+			let sum = ref(0)
+			let p = new Promise((resolve,reject)=>{
+				setTimeout(()=>{
+					resolve({sum})
+				},3000)
+			})
+			return await p
+		}
 	}
 </script>
 
