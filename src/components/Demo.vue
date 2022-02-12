0be2ee7ea6b8f9cef18790551d05ac7b0c11bc85
Jaunty-Liu <15613469426@163.com>
2022-02-12 15:51:16 +0800

    toRaw与markRaw

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,22 +1,20 @@
 <template>
-	<h4>当前求和为：{{sum}}</h4>
-	<button @click="sum++">点我++</button>
-	<hr>
-	<h2>姓名：{{name}}</h2>
-	<h2>年龄：{{age}}</h2>
-	<h2>薪资：{{job.j1.salary}}K</h2>
-	<button @click="name+='~'">修改姓名</button>
-	<button @click="age++">增长年龄</button>
-	<button @click="job.j1.salary++">涨薪</button>
+	<!-- 因为一开始person中没有car属性，所以这样处理 -->
+	<!-- 不建议给person写上“car:{}”，因为这样页面会显示{} -->
+	<h3 v-show="person.car">座驾信息：{{person.car}}</h3>
+	<button @click="showRawPerson">输出最原始的person</button>
+	<button @click="addCar">给人添加一台车</button>
+	<!-- 以下两行都可以实现响应式，因为新加的属性会自动设置为reactive -->
+	<!-- 但是设置为markRow就不能修改了 -->
+	<button @click="person.car.name+='!'">换车名</button>
+	<button @click="changePrice">换价格</button>
 </template>
 
 <script>
-	import {ref,reactive,toRefs,readonly,shallowReadonly} from 'vue'
+	import {reactive,toRefs,markRaw} from 'vue'
 	export default {
 		name: 'Demo',
 		setup(){
-			//数据
-			let sum = ref(0)
 			let person = reactive({
 				name:'张三',
 				age:18,
@@ -27,15 +25,24 @@
 				}
 			})
 
-			person = readonly(person)
-			// person = shallowReadonly(person)
-			// sum = readonly(sum)
-			// sum = shallowReadonly(sum)
+			function addCar(){
+				let car = {name:'奔驰',price:40}
+				// person.car = car
+				person.car = markRaw(car)
+			}
+
+			function changePrice(){
+				person.car.price++
+				console.log(person.car.price)
+			}
 
 			//返回一个对象（常用）
 			return {
-				sum,
-				...toRefs(person)
+				person,
+				...toRefs(person),
+				showRawPerson,
+				addCar,
+				changePrice
 			}
 		}
 	}
