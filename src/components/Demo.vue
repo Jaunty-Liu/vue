e1ee065a629f839e2b240acad865b767bce0ade2
Jaunty-Liu <15613469426@163.com>
2022-02-12 12:35:28 +0800

    readonly与shallowReadonly

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,12 +1,7 @@
 <template>
-	<h4>当前的x.y值是：{{x.y}}</h4>
-	<!-- 这个功能无用，因为value是NaN -->
-	<button @click="x++">点我x++</button>
-	<!-- 要用的话只能直接替换 -->
-	<button @click="x={y:888}">点我替换x</button>
-	<button @click="x.y++">点我x.y++</button>
+	<h4>当前求和为：{{sum}}</h4>
+	<button @click="sum++">点我++</button>
 	<hr>
-	<h4>{{person}}</h4>
 	<h2>姓名：{{name}}</h2>
 	<h2>年龄：{{age}}</h2>
 	<h2>薪资：{{job.j1.salary}}K</h2>
@@ -16,13 +11,13 @@
 </template>
 
 <script>
-	import {ref,reactive,toRef,toRefs,shallowReactive,shallowRef} from 'vue'
+	import {ref,reactive,toRefs,readonly,shallowReadonly} from 'vue'
 	export default {
 		name: 'Demo',
 		setup(){
-			//只考虑第一层数据的响应式，也就是按“涨薪”按钮之后不会响应，但是数据实际上被改变了，修改第一层数据其他几层数据就会响应
-			let person = shallowReactive({
-			// let person = reactive({
+			//数据
+			let sum = ref(0)
+			let person = reactive({
 				name:'张三',
 				age:18,
 				job:{
@@ -31,15 +26,15 @@
 					}
 				}
 			})
-			let x = shallowRef({
-				y:0
-			})
-			console.log('******',x)// value是NaN
+
+			person = readonly(person)
+			// person = shallowReadonly(person)
+			// sum = readonly(sum)
+			// sum = shallowReadonly(sum)
 
 			//返回一个对象（常用）
 			return {
-				x,
-				person,
+				sum,
 				...toRefs(person)
 			}
 		}
