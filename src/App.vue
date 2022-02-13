bcb880dbf13c286c2d27cd44a199cad2548a66ce
Jaunty-Liu <15613469426@163.com>
2022-02-13 13:15:50 +0800

    vue3v-bind和class和style实例：前两个特殊

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
-	<div class="static" :class="[activeClass, errorClass]"></div>
+	<ul>
+    	<li v-for="(item,index) in list" :key="index" :class="{'red':index==0,'blue':index==1}">{{item}}</li>
+    	<li v-for="(item,index) in list" :key="index" :style="{'color': color, 'blue': index == 1}">{{item}}</li>
+    	<li v-for="(item,index) in list" :key="index" :style="[index == 0? styleRed: null, index == 1? styleBlue: null]">{{item}}</li>
+	</ul>
 </template>
 
 <script>
 export default {
-	name: 'App',
-	data(){
-		return{
-			activeClass: 'active',
-			errorClass: 'error'
-		}
-	}
-}
+    name: "App",
+    data() {
+        return {
+          list: ['马总', '刘总', '李总'],
+		  styleRed: {
+			  color: 'red'
+		  },
+		  styleBlue: {
+			  color: 'blue'
+		  }
+        };
+    },
+};
 </script>
 
 <style>
-	.static {
-		font-size: 20px;
+	.red {
+		color: red;
 	}
 
-	.active {
-		width: 100px;
-		height: 100px;
-		background: red;
-	}
-
-	.error {
+	.blue {
 		color: blue;
 	}
 </style>
