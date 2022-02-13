43ff8f53660f068a232c24faf977a1b429d70c82
Jaunty-Liu <15613469426@163.com>
2022-02-13 13:16:24 +0800

    更正

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
 	<ul>
     	<li v-for="(item,index) in list" :key="index" :class="{'red':index==0,'blue':index==1}">{{item}}</li>
-    	<li v-for="(item,index) in list" :key="index" :style="{'color': color, 'blue': index == 1}">{{item}}</li>
     	<li v-for="(item,index) in list" :key="index" :style="[index == 0? styleRed: null, index == 1? styleBlue: null]">{{item}}</li>
 	</ul>
 </template>
