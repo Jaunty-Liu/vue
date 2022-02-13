425a89c35724e6909c2e90a12df65252ff485281
Jaunty-Liu <15613469426@163.com>
2022-02-13 12:52:46 +0800

    vue3v-bind结合class

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
-  <span>111</span>
-  <span>{{h2}}</span>
-  <span v-html="h2"></span>
+	<div class="static" :class="[activeClass, errorClass]"></div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data(){
-    return{
-      h2: "<h2>h2<h2>"
-    }
-  }
+	name: 'App',
+	data(){
+		return{
+			activeClass: 'active',
+			errorClass: 'error'
+		}
+	}
 }
 </script>
+
+<style>
+	.static {
+		font-size: 20px;
+	}
+
+	.active {
+		width: 100px;
+		height: 100px;
+		background: red;
+	}
+
+	.error {
+		color: blue;
+	}
+</style>
