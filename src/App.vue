b40239790cb98a56d251c73c6c75c8ac47eff562
Jaunty-Liu <15613469426@163.com>
2022-02-13 17:03:53 +0800

    原生数据绑定

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
-	<!-- <input type="text" @keyup="afn($event)"> -->
-	<!-- 也就是相当于 -->
-	<input type="text" @keyup.enter="afn1">
-</template>
+<h2>人员登记系统</h2>
 
+<div class="people_list">
+    <ul>
+        <li>姓名： <input type="text" id="username" /></li>
+        <li>年龄： <input type="text" ref="age" /></li>
+    </ul>
+    <button @click="doSubmit()" class="submit">获取表单的内容</button>
+</div>
+</template>
 <script>
 export default {
-    name: "App",
     data() {
         return {
+            message: "你好",
         };
     },
-	methods: {
-		afn(e){
-			console.log(e);
-			if(e.keyCode == 13){
-				console.log("按了回车键");
-			}
-		},
-		afn1(){
-			console.log("按了回车键");
-		}
-	},
+    methods: {
+        doSubmit() {
+            var username = document.querySelector("#username");
+            alert(username.value);
+
+            var ageDom = this.$refs.age;
+            alert(ageDom.value);
+        },
+    },
 };
 </script>
-
-<style>
-</style>
