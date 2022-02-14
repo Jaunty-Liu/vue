ff0a0767481bf3668bb29f81a452f0d3b190ddc8
Jaunty-Liu <15613469426@163.com>
2022-02-14 16:56:10 +0800

    keep-alive+切换

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,27 @@
 <template>
-     <select>
-        <!-- 默认选择的首选项是空并且不显示 -->
-        <option selected="selected" disabled="disabled"  style='display: none' value=''></option>
-        <option value="1">Yesterday</option>
-        <option value="2">Today</option>
-        <option value="3">Tomorrow</option>
-    </select>
+    <div class="home">
+        <button @click="isShow=!isShow">挂载/卸载组件</button>
+        <br>
+        <keep-alive>
+            <life-cycle v-if="isShow"></life-cycle>
+        </keep-alive>
+    </div>
 </template>
+
+<script>
+import LifeCycle from './LifeCycle';
+export default {
+    data() {
+        return {
+            isShow: true
+        }
+    },
+    methods: {
+
+    },
+    components: {
+        LifeCycle
+    }
+
+}
+</script>
